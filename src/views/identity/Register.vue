07bfaa9f9dea8408b7d6b735d06a96ecc579668d
KyuHwan Shim <46957634+underthelights@users.noreply.github.com>
2021-02-25 19:25:56 +0900

    Update Register.vue

--- src/views/identity/Register.vue
+++ src/views/identity/Register.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="container">
         <div id="menu">
-            <!-- logo !! -->
+            <!-- logo insert please!! -->
             <router-link to="/home/">Home</router-link>
         </div>
         <div class="school">
