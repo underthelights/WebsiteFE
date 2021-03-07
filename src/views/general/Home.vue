c925bbb4c669ff693778ab1d61cdec080f7dc2ae
KyuHwan Shim <46957634+underthelights@users.noreply.github.com>
2021-03-07 16:21:50 +0900

    Update Home.vue

--- src/views/general/Home.vue
+++ src/views/general/Home.vue
@@ -5,10 +5,15 @@
         <br><strong style='font-size:60px'>너울가지</strong><br>
         <span style='font-size:30px'>프로그래밍을 함께<br>프로젝트는 즐겁게</span><br>
         <a href="/" class="macbook">
+        <!-- main home screenshot here!! -->
         <img src="../../assets/image/MB.png" alt="neoul-gazi" width="50%"></a>
+        <br><strong style='font-size:60px'><br>프로젝트를 함께 진행할 <br>팀원을 찾으시나요?</strong><br>
+
+        <span style='font-size:30px'><br>더욱 발전할 당신의 프로그래밍</span><br>
     </div>
     </div>
-    <aside id="aside" style="display:table;background-color: #ffffff;margin-left: 50%;width: 50%;">
+    <aside id="aside" style="position:absolute;top:30%;margin-top:-50px;background-color: #ffffff;margin-left: 50%;width: 50%;">
+
         <div id="login" style='line-height:300% display:table-cell vertical-align:middle'>
             <a href="/" class="logo">    <img src="../../assets/image/logo.png" alt="neoul-gazi" width="17%"></a>
             <p style='line-height:150%'><br>
@@ -30,33 +35,6 @@
             </div>
         </div>
     </aside>
-<!-- 
-        <aside id="aside">
-        <div id="login" style='line-height:300% display:table-cell vertical-align:middle'>
-                <a href=“/“ class=“logo”>
-                <img src=“../../assets/image/logo.png” alt=“neoul-gazi” width=“17%”>
-                </a>
-                <p style=‘line-height:150%’>
-                    <br><strong style=‘font-size:60px’>너울가지</strong><br><br>
-                    <span style=‘line-height:200%; font-size:19px’>
-                        Hack your Lifes to Fantasy<br>Code your Dreams to Reality
-                    </span>
-                </p>
-        
-                <div id=“buttons” style=‘line-height:300%’>
-                    <b-button pill=“pill” :style="{width: '395px'}" variant=“outline-danger” to=“/login/“>
-                        <span style=‘font-size:19px’>로그인</span>
-                    </b-button><br>
-                    <b-button pill=“pill” :style="{width: '395px'}" variant=“danger” to=“/register/“>
-                        <span style=‘font-size:19px’>회원가입</span>
-                    </b-button>
-                    <p class=“find” style=‘line-height:140%’>
-                        <br><router-link to=“/apply-reset-id/“>아이디 찾기</router-link>|
-                        <router-link to=“/apply-reset-password/“>비밀번호 찾기</router-link>
-                    </p>
-                </div>
-            </div>
-        </aside> -->
 
     <footer id="footer" style="position:absolute;width:100%;bottom:0;height: 50px; ">
         <div id="footer-content" >
@@ -72,16 +50,6 @@
 
 <script>
     export default {
-        data() {
-            return {
-            }
-        },
-        methods:{
-          
-        },
-        computed: {
-          
-        }
     }
 </script>
 
@@ -91,13 +59,14 @@
   background-color: #d4d4d3;
   position: absolute;
   left: 0px;
+  height:100%;
   width: 50%;
 }
 
 .right-content{
-  display:table;
+ display: table-cell;
+    vertical-align: middle;
   background-color: #ffffff;
-  
   right: 0px;
   width: 50%;
 }
